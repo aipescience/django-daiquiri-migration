718c319c0c0046e5dfb8da8dc3546671e5d54f11
Jochen Klar <jochenklar@gmail.com>
2018-03-27 17:23:53 +0200

    Add contact script

---
+++ create_contact_fixtures.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import json
+import MySQLdb
+
+from datetime import timedelta
+
+from settings import DAIQUIRI_DATABASE
+
+# init fixture list (will be serialized to json at the end)
+fixtures = []
+
+# get a connection to the daiquiri database
+daiquiri_connection = MySQLdb.connect(**DAIQUIRI_DATABASE)
+daiquiri_cursor = daiquiri_connection.cursor()
+
+# query all messages from the daiquiri database
+daiquiri_cursor.execute('''
+    SELECT id, firstname, lastname, email, subject, message, `datetime`, status_id, user_id
+    FROM Contact_Messages
+''')
+
+# fetch users and create fixtures for every user, profile and email address (for allauth)
+for row in daiquiri_cursor.fetchall():
+    message_id, first_name, last_name, email, subject, message, created, status_id, user_id = row
+
+    author = '%s %s' % (first_name, last_name)
+    status = 'ACTIVE' if status_id == 1 else 'CLOSED'
+    created = (created - timedelta(hours=1)).isoformat() + 'Z'
+    user_id = user_id if user_id > 0 else None
+
+    # create a contactmessage fixture
+    fixtures.append({
+        'model': 'daiquiri_contact.contactmessage',
+        'pk': message_id,
+        'fields': {
+            'author': author,
+            'email': email,
+            'subject': subject,
+            'user': user_id,
+            'status': status,
+            'created': created,
+            'message': message
+        }
+    })
+
+print(json.dumps(fixtures, indent=2, sort_keys=True))
